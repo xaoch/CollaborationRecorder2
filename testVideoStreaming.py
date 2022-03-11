054144c5568ef101b7294598799a8e706be9d2c3
Xavier Ochoa <xavier.ochoa@gmail.com>
2022-03-11 11:55:52 -0500

    Adapting camera

--- testVideoStreaming.py
+++ testVideoStreaming.py
@@ -6,7 +6,7 @@ IPAddr = socket.gethostbyname(hostname+".local" )
 print ("IP",IPAddr)
 
 audio = ffmpeg.input("default",f="alsa",channels=1,sample_rate=44100)
-video = ffmpeg.input("/dev/video0",f="v4l2",input_format="h264",framerate=15)
+video = ffmpeg.input("/dev/video0",f="v4l2",framerate=15)
 out = ffmpeg.output(audio,video,"http://"+IPAddr+":8080",listen=1,f="flv",vcodec="copy")
 print(ffmpeg.get_args(out))
 ffmpeg.run(out)
