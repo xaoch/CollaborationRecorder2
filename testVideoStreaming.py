f8e326e60c62a9e68a8863e76c9242ecd1c39270
Xavier Ochoa <xavier.ochoa@gmail.com>
2022-03-11 15:51:49 -0500

    Record and Stream

--- testVideoStreaming.py
+++ testVideoStreaming.py
@@ -6,7 +6,7 @@ IPAddr = socket.gethostbyname(hostname+".local" )
 print ("IP",IPAddr)
 
 audio = ffmpeg.input("default",f="alsa",channels=1,sample_rate=44100)
-video = ffmpeg.input("/dev/video0",f="v4l2",video_size="1280x768",input_format="h264",framerate=15)
+video = ffmpeg.input("/dev/video0",f="v4l2",video_size="1024x768",input_format="h264",framerate=15)
 out1 = ffmpeg.output(audio,video,"http://"+IPAddr+":8080",listen=1,f="flv",vcodec="copy")
 out2 = ffmpeg.output(audio,video, "output.mp4", vcodec="copy")
 out=ffmpeg.merge_outputs(out1,out2)
