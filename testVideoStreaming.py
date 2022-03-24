8681c0d82229637258f0813a4da0cf39d73bc92f
Xavier Ochoa <xavier.ochoa@gmail.com>
2022-03-24 11:09:46 -0400

    Writing info in video

--- testVideoStreaming.py
+++ testVideoStreaming.py
@@ -1,11 +1,13 @@
 import ffmpeg
 import socket
-import time
+
 
 hostname = socket.gethostname()
 IPAddr = socket.gethostbyname(hostname+".local" )
 print ("IP",IPAddr)
 
+def time():
+    return "Hola"
 
 audio = ffmpeg.input("default",f="alsa",channels=1,sample_rate=44100)
 video = ffmpeg.input("/dev/video0",f="v4l2",input_format="h264",framerate=15)
