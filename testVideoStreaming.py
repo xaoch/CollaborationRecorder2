b607ec376ca5a2aa518921f6163965cf7aeda869
Xavier Ochoa <xavier.ochoa@gmail.com>
2022-03-24 12:03:46 -0400

    Writing info in video

--- testVideoStreaming.py
+++ testVideoStreaming.py
@@ -5,14 +5,16 @@ import socket
 hostname = socket.gethostname()
 IPAddr = socket.gethostbyname(hostname+".local" )
 print ("IP",IPAddr)
+int i=0
 
 def time():
-    return "Hola"
+    global i
+    i=i+1
+    print i
 
 audio = ffmpeg.input("default",f="alsa",channels=1,sample_rate=44100)
 video = ffmpeg.input("/dev/video0",f="v4l2",input_format="h264",video_size=(1280, 976),framerate=15)
-#video = video.drawtext(text=f"{time()}",fontsize=64, y=100, x=100,fontcolor='white',escape_text=True)
-#video = video.split()
+text = video.drawtext(text=f"{time()}",fontsize=64, y=100, x=100,fontcolor='white',escape_text=True)
 out1 = ffmpeg.output(audio,video,"rtmp://localhost:1935/live/1",f="flv", vcodec="copy")
 out2 = ffmpeg.output(audio,video, "output.mp4", vcodec="copy")
 out=ffmpeg.merge_outputs(out1,out2)
