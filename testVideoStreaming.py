8e842953f8660199281dad28fcaea989336f9513
Xavier Ochoa <xavier.ochoa@gmail.com>
2022-03-24 11:24:32 -0400

    Writing info in video

--- testVideoStreaming.py
+++ testVideoStreaming.py
@@ -11,7 +11,8 @@ def time():
 
 audio = ffmpeg.input("default",f="alsa",channels=1,sample_rate=44100)
 video = ffmpeg.input("/dev/video0",f="v4l2",input_format="h264",framerate=15)
-text = ffmpeg.drawtext(video,text=f"{time()}",fontsize=64, y=100, x=100,fontcolor='white',escape_text=True)
+video = video.split()
+text = ffmpeg.drawtext(video[0],text=f"{time()}",fontsize=64, y=100, x=100,fontcolor='white',escape_text=True)
 out1 = ffmpeg.output(audio,text,"rtmp://localhost:1935/live/1",f="flv",vcodec="copy")
 out2 = ffmpeg.output(audio,text, "output.mp4", vcodec="copy")
 out=ffmpeg.merge_outputs(out1,out2)
