25aa7bcc1328f8553fe380fd60d23fdae7ab0ed8
Xavier Ochoa <xavier.ochoa@gmail.com>
2022-03-29 16:05:44 -0400

    Writing info in video

--- main.py
+++ main.py
@@ -39,9 +39,9 @@ def start_streaming(recordingId):
     #out = ffmpeg.merge_outputs(out1, out2)
     audio = ffmpeg.input("sysdefault",  **{'async': 1},f="alsa", channels=1, sample_rate=44100)
     video = ffmpeg.input("/dev/video0", f="v4l2", input_format="h264", video_size=(1280, 976))
-    text = video.drawtext(textfile="doa.txt", reload=1, fontcolor="red", x=40, y=40, fontsize="64", escape_text=True)
+    #text = video.drawtext(textfile="doa.txt", reload=1, fontcolor="red", x=40, y=40, fontsize="64", escape_text=True)
     out1 = ffmpeg.output(audio, video, "rtmp://localhost:1935/live/1", f="flv", vcodec="copy")
-    out2 = ffmpeg.output(audio, text, filePath, preset="ultrafast")
+    out2 = ffmpeg.output(audio, video, filePath, vcodec="copy")
     #out3 = ffmpeg.output(text, doaPath, )
     out = ffmpeg.merge_outputs(out1, out2)
     global ffprocess
